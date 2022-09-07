fcd856ace4a8e7bceee88093e41706c83107d887
sushmitamaurya <mauryasushmita83@gmail.com>
2022-09-07 18:09:29 +0530

    filds changes

--- lib/home.dart
+++ lib/home.dart
@@ -9,55 +9,75 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Card(
-        child: Container(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
-            //margin: EdgeInsets.only(top: 10, bottom: 10, left: 15, right: 15),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.all(20),
-                  child: Card(
-                      margin: EdgeInsets.all(20),
-                      color: Colors.white,
-                      child: Text(
-                        "Darwin",
-                        style: TextStyle(fontSize: 20, color: Colors.blue),
-                      )),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  child: Column(
-                    children: [
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Name'),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Surname'),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Surname'),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      ElevatedButton(onPressed: () {}, child: Text("Submit"))
-                    ],
+          child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Color(0xffF9F9F9),
+              ),
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    child: Card(
+                        margin: EdgeInsets.all(20),
+                        color: Colors.white,
+                        child: Text(
+                          "Basic Information",
+                          style: TextStyle(fontSize: 20, color: Colors.black),
+                        )),
                   ),
-                ),
-              ],
-            )),
-      )),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(hintText: 'Name'),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(hintText: 'Surname'),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(hintText: 'Surname'),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(hintText: 'Surname'),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          height: 45,
+                          width: 400,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Add Inquiry",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.blue,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ))),
     );
   }
 }
