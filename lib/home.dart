c9c43f3df09c373a82af264d3cbd48f145910538
sushmitamaurya <mauryasushmita83@gmail.com>
2022-09-07 18:17:24 +0530

    filds contact

--- lib/home.dart
+++ lib/home.dart
@@ -54,7 +54,7 @@ class HomePage extends StatelessWidget {
                           height: 20,
                         ),
                         TextField(
-                          decoration: InputDecoration(hintText: 'Surname'),
+                          decoration: InputDecoration(hintText: 'Contact'),
                         ),
                         SizedBox(
                           height: 20,
